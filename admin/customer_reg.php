c75315276abd2fb6f16d99cd76e8d0ed10ccdfc9
ARSHAD KHAN <108625848+mrARSHADKHAN@users.noreply.github.com>
2023-07-13 00:14:48 +0530

    forms_update

--- admin/customer_reg.php
+++ admin/customer_reg.php
@@ -1,35 +1,15 @@
 <?php
   include("includes/header.php");
-  include("includes/topbar.php");
-  include("includes/sidebar.php");
+//   include("includes/topbar.php");
+//   include("includes/sidebar.php");
 ?>
 
 
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-<!-- Content Header (Page header) -->
-<div class="content-header">
-    <div class="container-fluid">
-    <div class="row mb-2">
-        <div class="col-sm-6">
-        <h1 class="m-0">Dashboard</h1>
-        </div><!-- /.col -->
-        <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-        </div><!-- /.col -->
-    </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-</div>
-<!-- /.content-header -->
-
-    <section class="content">
+<section class="content">
         <div class="container-fluid">
 
             <!-- Horizontal Form -->
-            <div class="card card-info">
+            <div class="card card-info m-5 mx-auto" style="width:70%;"> 
                 <div class="card-header">
                     <h3 class="card-title">Add Your Details</h3>
                 </div>
@@ -38,8 +18,46 @@
                 <!-- form start -->
                 <form class="form-horizontal">
                     <div class="card-body">
-                        <div class="form-group row">
-                            <label for="inputName">Name</label>
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6 col-lg-6">
+                                <div class="form-group ">
+                                    <label for="inputName">First Name</label>
+                                    <input type="text" id="inputName" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-6">
+                                <div class="form-group ">
+                                    <label for="inputName">Last Name</label>
+                                    <input type="text" id="inputName" class="form-control">
+                                </div>    
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6 col-lg-6">
+                                <div class="form-group ">
+                                    <label for="inputName">NIC</label>
+                                    <input type="text" id="inputName" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-6">
+                                <div class="form-group ">
+                                    <label for="inputName">Passport No.</label>
+                                    <input type="text" id="inputName" class="form-control">
+                                </div>    
+                            </div>
+                        </div>
+
+                        <div class="form-group ">
+                            <label for="inputName">NIC</label>
+                            <input type="text" id="" class="form-control">
+                        </div>
+                        <div class="form-group ">
+                            <label for="inputName">Passport No.</label>
+                            <input type="text" id="inputName" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputName">Address</label>
                             <input type="text" id="inputName" class="form-control">
                         </div>
 
@@ -47,8 +65,8 @@
                     <!-- /.card-body -->
 
                     <div class="card-footer">
-                        <button type="submit" class="btn btn-info">Sign in</button>
-                        <button type="submit" class="btn btn-default float-right">Cancel</button>
+                        <button type="submit" class="btn btn-info float-right">Save</button>
+                        <button type="submit" class="btn btn-default">Cancel</button>
                     </div>
                     <!-- /.card-footer -->
                 </form>
@@ -58,6 +76,8 @@
 
         </div><!-- /.container-fluid -->
     </section> 
+
+   
 
 
 
