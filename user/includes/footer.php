dfd2ac233895321d05d8da46ef90ffe67c3dbfae
ARSHAD KHAN <108625848+mrARSHADKHAN@users.noreply.github.com>
2023-07-21 17:33:02 +0530

    files_organize

--- user/includes/footer.php
+++ user/includes/footer.php
@@ -8,43 +8,43 @@
 </div>
 
 <!-- jQuery -->
-<script src="assets/plugins/jquery/jquery.min.js"></script>
+<script src="../admin/assets/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="../admin/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="assets/plugins/chart.js/Chart.min.js"></script>
+<script src="../admin/assets/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="assets/plugins/sparklines/sparkline.js"></script>
+<script src="../admin/assets/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
-<script src="assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="../admin/assets/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="../admin/assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="assets/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="../admin/assets/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- InputMask -->
-<script src="assets/plugins/inputmask/jquery.inputmask.min.js"></script>
+<script src="../admin/assets/plugins/inputmask/jquery.inputmask.min.js"></script>
 <!-- daterangepicker -->
-<script src="assets/plugins/moment/moment.min.js"></script>
-<script src="assets/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="../admin/assets/plugins/moment/moment.min.js"></script>
+<script src="../admin/assets/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="../admin/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="assets/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="../admin/assets/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="../admin/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- BS-Stepper -->
-<script src="assets/plugins/bs-stepper/js/bs-stepper.min.js"></script>
+<script src="../admin/assets/plugins/bs-stepper/js/bs-stepper.min.js"></script>
 <!-- AdminLTE App -->
-<script src="assets/js/adminlte.js"></script>
+<script src="../admin/assets/js/adminlte.js"></script>
 <!-- AdminLTE for demo purposes -->
-<!-- <script src="assets/js/demo.js"></script> -->
+<!-- <script src="../admin/assets/js/demo.js"></script> -->
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="assets/js/pages/dashboard.js"></script>
+<script src="../admin/assets/js/pages/dashboard.js"></script>
 
 <?php  
     include("includes/adv_script.php");
